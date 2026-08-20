0bb0e0184a03407c91b759fdea5ec180c962b82c
newlabs2021 <newlabs2021@gmail.com>
2026-08-20 16:17:56 +0530

    Update Header.tsx

--- Header.tsx
+++ Header.tsx
@@ -99,5 +99,17 @@ export function Header() {
         </div>
       ) : null}
     </header>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BYGZ44GG53"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BYGZ44GG53');
+</script>
+
+    
   );
 }
