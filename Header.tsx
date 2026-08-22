3bb8c87a9eba29efef56eabf69a40cce7b8e1815
newlabs2021 <newlabs2021@gmail.com>
2026-08-22 16:18:17 +0530

    Update Header.tsx

--- Header.tsx
+++ Header.tsx
@@ -24,21 +24,6 @@ export function Header() {
   return (
     <header
 
-
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-QBJJNCR73Y"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-QBJJNCR73Y');
-</script>
-
-
-
-      
       className={cn(
         "fixed inset-x-0 top-0 z-50 transition-all duration-300",
         scrolled
@@ -114,6 +99,19 @@ export function Header() {
           </nav>
         </div>
       ) : null}
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QBJJNCR73Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QBJJNCR73Y');
+</script>
+
+      
     
     </header>   
   );
